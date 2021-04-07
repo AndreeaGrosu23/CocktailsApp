1f58d90938e9a1b694ea89a924cff4d852ecab1a
AndreeaGrosu23 <andreea.grosu87@gmail.com>
2021-04-07 22:35:02 +0300

    Add signup feature using firebase authentication

---
+++ recipes/src/app/auth/auth.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error!: string;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if(!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading=true;
+    if(this.isLoginMode) {
+      //...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        responseData => {
+          console.log(responseData);
+          this.isLoading=false;
+        },
+        error => {
+          this.error='An error ocurred!';
+          this.isLoading=false;
+        }
+      );
+    }
+   
+    form.reset();
+  }
+
+}
